bc98d1de973b917c31b6fe68b94af8e50789e61f
Lior Rotberg <Exolite@gmail.com>
2021-11-28 16:45:29 +0200

    Update LeftMenuPage

--- src/main/java/pages/LeftMenuPage.java
+++ src/main/java/pages/LeftMenuPage.java
@@ -13,4 +13,35 @@ public class LeftMenuPage {
 
   @FindBy(css = "a[aria-label='Create']")
   protected WebElement linkCreateDashboard;
+
+  @FindBy(css = "a[aria-label='Explore']")
+  protected WebElement linkExplore;
+
+  @FindBy(css = "a[aria-label='Alerting']")
+  protected WebElement linkAlerting;
+
+  @FindBy(css = "a[aria-label='Configuration']")
+  protected WebElement linkConfig;
+
+  @FindBy(css = "a[aria-label='Server Admin']")
+  protected WebElement linkServerAdmin;
+
+  @FindBy(css = "a[aria-label='admin']")
+  protected WebElement linkProfile;
+
+  public WebElement getLinkHome() { return linkHome; }
+
+  public WebElement getBtnSearch() { return btnSearch; }
+
+  public WebElement getLinkCreateDashboard() { return linkCreateDashboard; }
+
+  public WebElement getLinkExplore() { return linkExplore; }
+
+  public WebElement getLinkAlerting() { return linkAlerting; }
+
+  public WebElement getLinkConfig() { return linkConfig; }
+
+  public WebElement getLinkServerAdmin() { return linkServerAdmin; }
+
+  public WebElement getLinkProfile() { return linkProfile; }
 }
