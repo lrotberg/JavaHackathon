7b283f3e39138b53b099c6638abf3bcbec35cc92
michal <mmichal1610@gmail.com>
2021-11-28 13:48:00 +0200

    Entering data (user name and password) to login page

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,6 +1,24 @@
 package pages;
-
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import utilities.CommonOps;
 
 public class LoginPage extends CommonOps {
+
+    @FindBy(how = How.XPATH, using= "//input[@name='user']")
+    protected WebElement userNameTxt;
+
+    @FindBy(how = How.XPATH, using= "//input[@id='current-password']")
+    protected WebElement passwordTxt;
+
+    @FindBy(how = How.XPATH, using= "//button[@class='css-1xivtx2-button']")
+    protected WebElement loginBtn;
+
+    public void EnterData() //String user, String pass
+    {
+        userNameTxt.sendKeys("admin");
+        passwordTxt.sendKeys("admin");
+        loginBtn.click();
+    }
 }
