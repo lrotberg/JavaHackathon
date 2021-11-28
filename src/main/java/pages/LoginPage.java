03d5755fcc53cf9c524b8c54d725a0c174527288
michal <mmichal1610@gmail.com>
2021-11-28 15:52:58 +0200

    adding getters to login page elements

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,10 +1,11 @@
 package pages;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-import utilities.CommonOps;
 
-public class LoginPage extends CommonOps {
+
+public class LoginPage  {
 
     @FindBy(how = How.XPATH, using= "//input[@name='user']")
     protected WebElement userNameTxt;
@@ -15,10 +16,36 @@ public class LoginPage extends CommonOps {
     @FindBy(how = How.XPATH, using= "//button[@class='css-1xivtx2-button']")
     protected WebElement loginBtn;
 
-    public void EnterData() //String user, String pass
+   /* public void EnterData() //String user, String pass
     {
         userNameTxt.sendKeys("admin");
         passwordTxt.sendKeys("admin");
         loginBtn.click();
+    }
+    @Step("entering data to element ")
+    public static void updateText(WebElement elem)
+    {
+        elem.sendKeys("admin");
+    }
+
+    @Step("click login button ")
+    public static void clickLoginBtn(WebElement elem)
+    {
+        elem.click();
+    }*/
+    @Step ("get user web element")
+    private  WebElement getUser()
+    {
+        return userNameTxt;
+    }
+    @Step ("get password web element")
+    private  WebElement getPass()
+    {
+        return passwordTxt;
+    }
+    @Step ("get login button web element")
+    private  WebElement getloginBtn()
+    {
+        return loginBtn;
     }
 }
