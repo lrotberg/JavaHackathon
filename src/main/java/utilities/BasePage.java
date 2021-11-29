674e075b3c00d19a2ddc154a3b7ee80f3029ffc8
michal <mmichal1610@gmail.com>
2021-11-29 15:57:57 +0200

    adding mobile

--- src/main/java/utilities/BasePage.java
+++ src/main/java/utilities/BasePage.java
@@ -1,10 +1,20 @@
 package utilities;
 
+import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BasePage {
 
   protected static WebDriver driver;
-  protected  static Actions action;
+  protected static Actions action;
+
+/***** mobile variables ***/
+  protected static String reportDirectory = "reports";
+  protected static String reportFormat = "xml";
+  protected static String testName = "Untitled";
+  protected static AndroidDriver mobileDriver;
+
+  protected static DesiredCapabilities dc;
 }
--- src/main/java/utilities/CommonOps.java
+++ src/main/java/utilities/CommonOps.java
@@ -1,5 +1,8 @@
 package utilities;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
@@ -8,10 +11,14 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class CommonOps extends BasePage {
 
@@ -31,12 +38,25 @@ public class CommonOps extends BasePage {
   public void closeWebSession() {
     driver.quit();
   }
+@Step ("open mobile app")
+public void openApp() throws MalformedURLException {
+  dc = new DesiredCapabilities();
+  dc.setCapability("reportDirectory", reportDirectory);
+  dc.setCapability("reportFormat", reportFormat);
+  dc.setCapability("testName", testName);
+  dc.setCapability(MobileCapabilityType.UDID, "R58R34SLXBD");
+  dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "kr.sira.unit");
+  dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".Intro");
+  mobileDriver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), dc);
+  MobileManagePages.buildPagesAppium();
 
+
+}
   @BeforeClass
-  public void startup() {
+  public void startup() throws MalformedURLException {
 
 
-
+    openApp();
     openWebSession();
   }
 
